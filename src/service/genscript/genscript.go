5dcccc066abc7852ea3220deba9b3b4229417b46
Eswar Rajan Subramanian <eswar@accuknox.com>
2021-12-07 16:14:32 +0530

    Enable secure gRPC and share ca-cert over installation script
    Signed-off-by: Eswar Rajan Subramanian <eswar@accuknox.com>

--- src/service/genscript/genscript.go
+++ src/service/genscript/genscript.go
@@ -37,12 +37,12 @@ func addContent(content string) {
 	ScriptData = ScriptData + content + "\n"
 }
 
-func GenerateEWInstallationScript(externalWorkload, identity string) string {
+func GenerateVMInstallationScript(virtualMachine string, identity string) string {
 
 	ScriptData = ""
 
 	kg.Printf("Generating the installation script =>")
-	kg.Printf("ClusterIP:%s ClusterPort:%d ewName:%s identity:%s", p.ipAddress, p.port, externalWorkload, identity)
+	kg.Printf("ClusterIP:%s ClusterPort:%d vmName:%s identity:%s", p.ipAddress, p.port, virtualMachine, identity)
 
 	addContent("#!/bin/bash")
 	addContent("set -e")
@@ -54,6 +54,13 @@ func GenerateEWInstallationScript(externalWorkload, identity string) string {
 	addContent(contentStr)
 	contentStr = "CLUSTER_IP=" + p.ipAddress
 	addContent(contentStr)
+	addContent("CA_CERT_FILE_PATH_LOCAL=\"/home/$USER/ca.cert\"")
+	addContent("CA_CERT_FILE_PATH_DEST=\"/media/root/ca.cert\"")
+	addContent("")
+	contentStr = "CA_CERT=" + p.CaCert
+	addContent(contentStr)
+	addContent("")
+	addContent("echo \"$CA_CERT\" > $CA_CERT_FILE_PATH_LOCAL")
 	addContent("")
 	contentStr = "CA_CERT=" + p.CaCert
 	addContent(contentStr)
@@ -67,14 +74,15 @@ func GenerateEWInstallationScript(externalWorkload, identity string) string {
 	addContent("fi")
 	addContent("")
 
-	contentStr = "WORKLOAD_IDENTITY=" + identity
+	contentStr = "VM_IDENTITY=" + identity
 	addContent(contentStr)
 	addContent("")
 
 	addContent("DOCKER_OPTS=\" -d -p 32767:32767 --log-driver syslog --restart always\"")
 	addContent("DOCKER_OPTS+=\" --privileged --add-host kvms.kubearmor.io:$CLUSTER_IP\"")
 	addContent("DOCKER_OPTS+=\" --env CLUSTER_PORT=$CLUSTER_PORT --env CLUSTER_IP=$CLUSTER_IP\"")
-	addContent("DOCKER_OPTS+=\" --env  WORKLOAD_IDENTITY=$WORKLOAD_IDENTITY\"")
+	addContent("DOCKER_OPTS+=\" --env VM_IDENTITY=$VM_IDENTITY\"")
+	addContent("DOCKER_OPTS+=\" --env CA_CERT_FILE_PATH=$CA_CERT_FILE_PATH_DEST\"")
 	addContent("DOCKER_OPTS+=\" --volume /var/run/docker.sock:/var/run/docker.sock\"")
 	addContent("DOCKER_OPTS+=\" --volume /usr/src:/usr/src\"")
 	addContent("DOCKER_OPTS+=\" --volume /lib/modules:/lib/modules\"")
@@ -82,6 +90,7 @@ func GenerateEWInstallationScript(externalWorkload, identity string) string {
 	addContent("DOCKER_OPTS+=\" --volume /sys/kernel/debug:/sys/kernel/debug\"")
 	addContent("DOCKER_OPTS+=\" --volume /etc/apparmor.d:/etc/apparmor.d\"")
 	addContent("DOCKER_OPTS+=\" --volume /etc/os-release:/media/root/etc/os-release\"")
+	addContent("DOCKER_OPTS+=\" --volume $CA_CERT_FILE_PATH_LOCAL:CA_CERT_FILE_PATH_DEST\"")
 	addContent("")
 	addContent("KUBEARMOR_OPTS=\" -enableKubeArmorVm true -logPath=/tmp/kubearmor.log\"")
 	addContent("")
