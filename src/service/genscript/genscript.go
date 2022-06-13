465e5bd44ab50eeee1b84fcf30f76f81ac28e283
Wazir Ahmed <wazir@accuknox.com>
2022-06-13 09:52:37 +0530

    Updated cilium-agent flags in the VM installation script
    Signed-off-by: Wazir Ahmed <wazir@accuknox.com>

--- src/service/genscript/genscript.go
+++ src/service/genscript/genscript.go
@@ -99,7 +99,7 @@ echo "Installing Cilium agent..."
 	cilium = `
 CILIUM_IMAGE=${CILIUM_IMAGE:-accuknox/cilium:latest}
 HOST_IF=${HOST_IF:-eth+,en+}
-CILIUM_CONFIG=${CILIUM_CONFIG:---devices=$HOST_IF --enable-host-firewall --enable-hubble=true --hubble-listen-address=:4244 --hubble-disable-tls=true --external-workload --enable-well-known-identities=false}
+CILIUM_CONFIG=${CILIUM_CONFIG:---devices=$HOST_IF --enable-host-firewall --restore=false --tunnel=disabled --ipv4-native-routing-cidr=0.0.0.0/0 --enable-hubble=true --hubble-listen-address=:4244 --hubble-disable-tls=true --external-workload --enable-well-known-identities=false}
 
 sudo mkdir -p /var/lib/cilium/etcd
 sudo tee /var/lib/cilium/etcd/config.yaml <<EOF >/dev/null
